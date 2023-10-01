7d94331d5a7bf0d819ccf76a41c5b378ebd360ec
harshit-jain52 <harshitjain0502@gmail.com>
2023-10-01 20:07:28 +0530

    BST functions

--- ALGOs/implementations_in_C/binary_trees/BST.h
+++ ALGOs/implementations_in_C/binary_trees/BST.h
@@ -76,7 +76,7 @@ node *deleteNode(node *root, int val) // O(depth)
         }
         else
         {
-            node *MaxInLeft = getMax(root->left);                 // finds the biggest node in the left branch (inorder predecessor)
+            node *MaxInLeft = getMax(root->left);                 // finds the biggest node in the left branch
             root->data = MaxInLeft->data;                         // copy data
             root->left = deleteNode(root->left, MaxInLeft->data); // recursively delete the max node in left
 
@@ -94,7 +94,7 @@ node *deleteNode(node *root, int val) // O(depth)
             /*
             Alternatively,
 
-            node *MinInRight = getMax(root->right);                 // finds the smallest node in the left branch (inorder successor)
+            node *MinInRight = getMax(root->right);                 // finds the smallest node in the left branch
             root->data = MinInRight->data;                          // copy data
             root->left = deleteNode(root->right, MinInRight->data); // recursively delete the min node in right
             */
@@ -104,12 +104,12 @@ node *deleteNode(node *root, int val) // O(depth)
     return root;
 }
 
-int findNode(node *root, int key) // O(depth)
+node *findNode(node *root, int key) // O(depth)
 {
     if (root == NULL)
-        return 0;
+        return NULL;
     if (root->data == key)
-        return 1;
+        return root;
     if (root->data > key)
         return findNode(root->left, key);
     if (root->data < key)
@@ -124,4 +124,92 @@ void printSorted(node *root) // In-Order Traversal of BST; O(N)
     printSorted(root->left);
     printf("\t[ %d ]\t", root->data);
     printSorted(root->right);
+}
+
+node *inOrderSuccessor(node *root, int key)
+{
+    node *succ = NULL;
+    node *curr = root;
+
+    while (curr)
+    {
+        if (curr->data > key)
+        {
+            succ = curr;
+            curr = curr->left;
+        }
+        else
+            curr = curr->right;
+    }
+
+    return succ;
+}
+
+node *inOrderPredecessor(node *root, int key)
+{
+    node *pre = NULL;
+    node *curr = root;
+
+    while (curr)
+    {
+        if (curr->data < key)
+        {
+            pre = curr;
+            curr = curr->right;
+        }
+        else
+            curr = curr->left;
+    }
+
+    return pre;
+}
+
+node *kthSmallest(node *root, int *k) // pointer to integer is passed
+{
+    if (root == NULL)
+        return NULL;
+
+    node *l = kthSmallest(root->left, k);
+    if (l != NULL)
+        return l;
+
+    *k = *k - 1;
+    if (*k == 0)
+        return root;
+
+    return kthSmallest(root->right, k);
+}
+
+node *kthLargest(node *root, int *k) // pointer to integer is passed
+{
+    if (root == NULL)
+        return NULL;
+
+    node *r = kthLargest(root->right, k);
+    if (r != NULL)
+        return r;
+
+    *k = *k - 1;
+    if (*k == 0)
+        return root;
+
+    return kthLargest(root->left, k);
+}
+
+node *LCA(node *root, node *p, node *q) // Lowest Common Ancestor
+{
+    if (p->data > q->data)
+        return LCA(root, q, p);
+
+    node *curr = root;
+
+    while (!(curr->data >= p->data && curr->data <= q->data))
+    {
+        if (curr->data < p->data)
+            curr = curr->right;
+        else
+            curr = curr->left;
+    }
+
+    return curr;
 }
