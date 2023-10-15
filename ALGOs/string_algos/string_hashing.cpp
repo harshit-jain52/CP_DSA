d32f47108f6dc145898a40edae1099959b592b33
harshit-jain52 <harshitjain0502@gmail.com>
2023-10-15 23:10:24 +0530

    Hashing Strings

--- ALGOs/string_algos/string_hashing.cpp
+++ ALGOs/string_algos/string_hashing.cpp
@@ -1,6 +1,3 @@
-/*
-https://cp-algorithms.com/string/string-hashing.html
-*/
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -8,7 +5,7 @@ typedef long long ll;
 const int p = 31;
 const int M = 1e9 + 9;
 
-ll coumpute_hash(string &s)
+ll compute_hash(string &s) // https://cp-algorithms.com/string/string-hashing.html
 {
     ll hash_value = 0;
     ll p_pow = 1;
@@ -17,6 +14,17 @@ ll coumpute_hash(string &s)
     {
         hash_value = (hash_value + (c - 'a' + 1) * p_pow) % M;
         p_pow = (p_pow * p) % M;
+    }
+
+    return hash_value;
+}
+
+ll hashStr(string &s) // Horner's Method
+{
+    ll hash_value = 0;
+    for (char c : s)
+    {
+        hash_value = (hash_value * p + (c - 'a' + 1)) % M;
     }
 
     return hash_value;
