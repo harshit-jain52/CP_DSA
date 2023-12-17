71f51bcb92fe17d576e6a92135e69bb074e7d8e3
harshit-jain52 <harshitjain0502@gmail.com>
2023-12-17 23:23:05 +0530

    Linked Lists

---
+++ ALGOs/linked-list/DLL.h
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+} Node;
+
+Node *makeNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node *));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    return newNode;
+}
+
+Node *arrayToDLL(int arr[], int sz) // O(N)
+{
+    if (sz <= 0)
+        return NULL;
+
+    Node *head = makeNode(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < sz; i++)
+    {
+        Node *tmp = makeNode(arr[i]);
+        tmp->prev = mover;
+        mover->next = tmp;
+        mover = tmp;
+    }
+
+    return head;
+}
+
+Node *deleteHead(Node *head) // O(1)
+{
+    if (head == NULL)
+        return head;
+
+    Node *back = head;
+    head = head->next;
+
+    if (head)
+        head->prev = NULL;
+
+    free(back);
+    return head;
+}
+
+Node *deleteTail(Node *head) // O(1)
+{
+    if (head == NULL)
+        return head;
+    if (head->next == NULL)
+    {
+        free(head);
+        return NULL;
+    }
+
+    Node *tail = head;
+    while (tail->next)
+        tail = tail->next;
+
+    Node *back = tail->prev;
+    back->next = NULL;
+    free(tail);
+
+    return head;
+}
+
+Node *deletePosition(Node *head, int pos) // O(N)
+{
+    if (head == NULL)
+        return head;
+
+    Node *tmp = head;
+
+    int ct = 0;
+    while (tmp)
+    {
+        ct++;
+        if (ct == pos)
+            break;
+        tmp = tmp->next;
+    }
+
+    if (ct < pos)
+        return head;
+
+    Node *back = tmp->prev;
+    Node *front = tmp->next;
+
+    if (back == NULL && front == NULL)
+    {
+        free(tmp);
+        return NULL;
+    }
+
+    if (back == NULL)
+        return deleteHead(head);
+
+    if (front == NULL)
+        return deleteTail(head);
+
+    back->next = front;
+    front->prev = back;
+    free(tmp);
+    return head;
+}
+
+void deleteNode(Node *node) // node can't be head; O(1)
+{
+    Node *back = node->prev;
+    Node *front = node->next;
+
+    back->next = front;
+
+    if (front != NULL)
+        front->prev = back;
+
+    free(node);
+}
+
+Node *insertBeforeHead(Node *head, int el) // O(1)
+{
+    Node *newHead = makeNode(el);
+    newHead->next = head;
+    if (head)
+        head->prev = newHead;
+
+    return newHead;
+}
+
+Node *insertAfterTail(Node *head, int el) // O(N)
+{
+    if (head == NULL)
+        return makeNode(el);
+
+    Node *tail = head;
+    while (tail->next)
+    {
+        tail = tail->next;
+    }
+
+    Node *newTail = makeNode(el);
+    newTail->prev = tail;
+    tail->next = newTail;
+
+    return head;
+}
+
+Node *insertBeforePos(Node *head, int el, int pos) // O(N)
+{
+    if (pos == 1)
+        return insertBeforeHead(head, el);
+
+    Node *tmp = head;
+    int ct = 0;
+    while (tmp->next)
+    {
+        ct++;
+        if (ct == pos)
+            break;
+        tmp = tmp->next;
+    }
+
+    if (ct < pos)
+        return head;
+
+    Node *back = tmp->prev;
+    Node *newNode = makeNode(el);
+    newNode->prev = back;
+    newNode->next = tmp;
+    back->next = newNode;
+    tmp->prev = newNode;
+
+    return head;
+}
+
+void *insertBeforeNode(Node *node, int el) // node can't be head; O(1)
+{
+    Node *back = node->prev;
+    Node *newNode = makeNode(el);
+    newNode->next = node;
+    newNode->prev = back;
+    back->next = newNode;
+    node->prev = newNode;
+}
+
+Node *reverseDLL(Node *head)
+{
+    Node *cur = head;
+    while (cur)
+    {
+        Node *back = cur->prev;
+        Node *front = cur->next;
+
+        cur->next = back;
+        cur->prev = front;
+
+        if (front == NULL)
+            break;
+        cur = front;
+    }
+
+    return cur;
+
+    // if (head == NULL || head->next == NULL)
+    //     return head;
+
+    // Node *back = NULL;
+    // Node *cur = head;
+
+    // while (cur)
+    // {
+    //     back = cur->prev;
+
+    //     cur->prev = cur->next;
+    //     cur->next = back;
+
+    //     cur = cur->prev;
+    // }
+
+    // return back->prev;
+}
---
+++ ALGOs/linked-list/LL.h
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} Node;
+
+Node *makeNode(int data)
+{
+    Node *newNode = (Node *)malloc(sizeof(Node *));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+Node *arrayToLL(int arr[], int sz) // O(N)
+{
+    if (sz <= 0)
+        return NULL;
+
+    Node *head = makeNode(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < sz; i++)
+    {
+        Node *tmp = makeNode(arr[i]);
+        mover->next = tmp;
+        mover = tmp;
+    }
+
+    return head;
+}
+
+void printLL(Node *head) // O(N)
+{
+    Node *mover = head;
+    while (mover)
+    {
+        printf("%d ", mover->data);
+        mover = mover->next;
+    }
+}
+
+int lengthOfLL(Node *head) // O(N)
+{
+    int len = 0;
+    Node *mover = head;
+    while (mover)
+    {
+        len++;
+        mover = mover->next;
+    }
+
+    return len;
+}
+
+int searchinLL(Node *head, int value) // O(N)
+{
+    Node *mover = head;
+
+    while (mover)
+    {
+        if (mover->data == value)
+            return 1;
+
+        mover = mover->next;
+    }
+
+    return 0;
+}
+
+Node *deleteHead(Node *head) // O(1);
+{
+    if (head == NULL)
+        return head;
+
+    Node *tmp = head;
+    head = head->next;
+    free(tmp);
+    
+    return head;
+}
+
+Node *deleteTail(Node *head) // O(N)
+{
+    if (head == NULL)
+        return head;
+    if (head->next == NULL)
+    {
+        free(head);
+        return NULL;
+    }
+
+    Node *mover = head;
+    while (mover->next->next)
+        mover = mover->next;
+
+    Node *tail = mover->next;
+    mover->next = NULL;
+    free(tail);
+
+    return head;
+}
+
+Node *deletePosition(Node *head, int pos) // O(N)
+{
+    if (head == NULL)
+        return head;
+
+    if (pos == 1)
+    {
+        Node *tmp = head;
+        head = head->next;
+        free(tmp);
+        return head;
+    }
+
+    Node *tmp = head;
+    Node *prev = NULL;
+
+    int ct = 0;
+    while (tmp)
+    {
+        ct++;
+        if (ct == pos)
+        {
+            prev->next = tmp->next;
+            free(tmp);
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    return head;
+}
+
+Node *deleteValue(Node *head, int val) // O(N)
+{
+    if (head == NULL)
+        return head;
+
+    if (head->data == val)
+    {
+        Node *tmp = head;
+        head = head->next;
+        free(tmp);
+        return head;
+    }
+
+    Node *tmp = head;
+    Node *prev = NULL;
+
+    while (tmp)
+    {
+        if (tmp->data == val)
+        {
+            prev->next = tmp->next;
+            free(tmp);
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+    return head;
+}
+
+Node *insertHead(Node *head, int el) // O(1)
+{
+    Node *newNode = makeNode(el);
+    newNode->next = head;
+    return newNode;
+}
+
+Node *insertTail(Node *head, int el) // O(N)
+{
+    if (head == NULL)
+        return makeNode(el);
+
+    Node *mover = head;
+    while (mover->next)
+        mover = mover->next;
+
+    mover->next = makeNode(el);
+
+    return head;
+}
+
+Node *insertPosition(Node *head, int el, int pos) // O(N)
+{
+    if (pos == 1)
+    {
+        Node *newNode = makeNode(el);
+        newNode->next = head;
+        return newNode;
+    }
+
+    Node *tmp = head;
+
+    int ct = 0;
+    while (tmp)
+    {
+        ct++;
+        if (ct == pos - 1)
+        {
+            Node *newNode = makeNode(el);
+            newNode->next = tmp->next;
+            tmp->next = newNode;
+            break;
+        }
+    }
+
+    return head;
+}
+
+Node *insertValue(Node *head, int el, int val) // insert el before val; O(N)
+{
+    if (head == NULL)
+        return head;
+
+    if (head->data == val)
+    {
+        Node *newNode = makeNode(el);
+        newNode->next = head;
+        return newNode;
+    }
+
+    Node *tmp = head;
+    while (tmp->next)
+    {
+        if (tmp->next->data == val)
+        {
+            Node *newNode = makeNode(el);
+            newNode->next = tmp->next;
+            tmp->next = newNode;
+            break;
+        }
+    }
+
+    return head;
+}
