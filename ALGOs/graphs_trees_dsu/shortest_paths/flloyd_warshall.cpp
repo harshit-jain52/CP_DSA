d1551cf44247c275cd00cac7b66ad248d387a56d
harshit-jain52 <harshitjain0502@gmail.com>
2024-05-16 23:47:04 +0530

    Flloyd Warshall

--- ALGOs/graphs_trees_dsu/shortest_paths/flloyd_warshall.cpp
+++ ALGOs/graphs_trees_dsu/shortest_paths/flloyd_warshall.cpp
@@ -7,6 +7,14 @@ const int N = 510;
 const int INF = 1e7 + 10;
 
 int dist[N][N];
+int par[N][N]; // for printing shortest path
+
+void printPath(int i, int j)
+{
+    if (i != j)
+        printPath(i, par[i][j]);
+    cout << j << " ";
+}
 
 int main()
 {
@@ -14,11 +22,8 @@ int main()
     {
         for (int j = 0; j < N; j++)
         {
-            if (i == j)
-                dist[i][j] = 0;
-
-            else
-                dist[i][j] = INF;
+            dist[i][j] = INF;
+            par[i][j] = i;
         }
     }
 
@@ -39,10 +44,31 @@ int main()
             for (int j = 1; j <= n; j++)
             {
                 if (dist[i][k] != INF && dist[k][j] != INF) // handle negative weights
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                {
+                    if (dist[i][j] < dist[i][k] + dist[k][j])
+                    {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        par[i][j] = par[k][j];
+                    }
+                }
             }
         }
     }
 
     // O(V^3)
+
+    // Finding Length of Shortest Cycle
+    int len = INF;
+    for (int i = 1; i <= n; i++)
+        len = min(len, dist[i][i]);
+
+    // Negative Cycle Detection
+    if (len < 0)
+        cout << "Negative Cycle Detected\n";
+
+    // Diameter of Graph -> longest shortest path
+    int diameter = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+            diameter = max(diameter, dist[i][j]);
 }
